d9de5e90ea91fb85498750971fc182a9c989e7b4
NeverMind109 <vladgladkih123@gmail.com>
2021-12-07 22:37:29 +0300

    Created: Added Search Section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(function(){
         infinite: true,
         speed: 300,
         slidesToShow: 1,
+        fade: true,
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="Next Arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="Previous Arrow"></button>',
       });
