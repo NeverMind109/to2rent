ea574dc4dc081f6dee5a7dc8b202f582d9692830
NeverMind109 <vladgladkih123@gmail.com>
2021-12-07 22:04:26 +0300

    Created: Added Slider Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="Next Arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="Previous Arrow"></button>',
+      });
 });
