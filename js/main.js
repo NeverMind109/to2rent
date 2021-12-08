c34976f4965a1e5aaa0a6ee1360b17497944ac4b
NeverMind109 <vladgladkih123@gmail.com>
2021-12-09 00:24:42 +0300

    Created: Added Adaptive Response

--- js/main.js
+++ js/main.js
@@ -7,5 +7,13 @@ $(function(){
         fade: true,
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="Next Arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="Previous Arrow"></button>',
+        responsive: [
+          {
+            breakpoint: 441,
+            settings: {
+              arrows: false,
+            }
+          },
+        ]
       });
 });
